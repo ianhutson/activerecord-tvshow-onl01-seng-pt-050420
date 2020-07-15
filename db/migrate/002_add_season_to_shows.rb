4ba4e5541fb1f401e9084580c33c3bddb3e6d576
Ian Hutson <ih00209@georgiasouthern.edu>
2020-07-15 17:44:15 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,11 +1,5 @@
-class CreateMovies < ActiveRecord::Migration[5.2]
+class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
-    create_table :movies do |t|
-      t.string   :title
-      t.integer  :release_date
-      t.string   :director
-      t.string   :lead
-      t.boolean  :in_theaters
-    end
+    add_column :shows, :season, :string
   end
 end
